fb33b0790ac8c47dd0bf6d5fd056c23ca3ab1641
zizo <zizo.toiawase@gmail.com>
2021-05-17 01:20:26 +0900

    update

--- setting.sh
+++ setting.sh
@@ -5,6 +5,7 @@ xcode-select --install
 
 ## install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 ## install ansible
 brew install ansible
